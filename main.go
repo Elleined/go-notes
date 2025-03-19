fc80e7ba31e7cc136102fa4d778c037e52303ddc
Denielle Mar M. De Guzman <demadegu@gmail.com>
2025-03-19 10:31:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	variableTopic()
+	dataTypesTopic()
+	decisionMakingAndControlFlowTopic()
+}
+
+func variableTopic() {
 	fmt.Println("=============================================")
 	fmt.Println("LONG HAND AND SHORT HAND VARIABLE DECLARATION")
 	fmt.Println("=============================================")
 	longHandAndShortHandVariableDeclaration()
 	fmt.Println("================== END LONG HAND AND SHORT HAND VARIABLE DECLARATION ==================")
 
+	fmt.Println("\n\n=============================================")
+	fmt.Println("VAR and CONST KEYWORD FOR DECLARING VARIABLE")
+	fmt.Println("=============================================")
+	varAndConst()
+	fmt.Println("================== END VAR and CONST KEYWORD FOR DECLARING VARIABLE ==================")
+}
+
+func dataTypesTopic() {
 	fmt.Println("\n\n==================")
 	fmt.Println("NUMERIC DATA TYPES")
 	fmt.Println("==================")
@@ -26,38 +40,57 @@ func main() {
 	fmt.Println("=========================")
 	dataStructureDataType()
 	fmt.Println("================== END DATA STRUCTURE DATA TYPE ==================")
+}
 
-	fmt.Println("\n\n==================")
-	fmt.Println("SPECIAL DATA TYPES")
-	fmt.Println("==================")
-	specialDataType()
-	fmt.Println("================== END OF SPECIAL DATA TYPE ==================")
+func decisionMakingAndControlFlowTopic() {
+	fmt.Println("\n\n=========================")
+	fmt.Println("DECISION MAKING")
+	fmt.Println("=========================")
+	decisionMaking()
+	fmt.Println("================== END DECISION MAKING ==================")
+
+	fmt.Println("\n\n=========================")
+	fmt.Println("CONTROL FLOW")
+	fmt.Println("=========================")
+	controlFlow()
+	fmt.Println("================== END CONTROL FLOW ==================")
 }
 
 func longHandAndShortHandVariableDeclaration() {
-	// Declaring long hand syntax
+	fmt.Print("\t === long hand === \n")
 	var longHand string = "Denielle"
-	fmt.Printf("Printed using long hand declaration: %s \n", longHand)
+	fmt.Printf("\t long hand declaration: %s \n", longHand)
 
-	// Declaring short hand syntax
+	fmt.Print("\t === short hand === \n")
 	shortHand := "Denielle"
-	fmt.Printf("Printed using short hand declaration: %s \n", shortHand)
+	fmt.Printf("\t short hand declaration: %s \n", shortHand)
+}
+
+func varAndConst() {
+	fmt.Print("\t === var === \n")
+	var name string = "Red"
+	fmt.Printf("\t Variable created via var keyword: %s \n", name)
+
+	fmt.Print("\t === const === \n")
+	const AGE uint8 = 22
+	fmt.Printf("\t Variable created via const keyword: %d \n", AGE)
 }
 
 func numericDataType() {
+	fmt.Println("\t === Integer ===")
 	var signedInt8 int8 = 127
-	fmt.Printf("\t Signed Integer 8 with range of -128 to 127: %v \n", signedInt8)
+	fmt.Printf("\t Integer 8 with range of -128 to 127: %v \n", signedInt8)
 
 	var signedInt16 int16 = 32767
-	fmt.Printf("\t Signed Integer 16 with range of -32,768 to 32767: %v \n", signedInt16)
+	fmt.Printf("\t Integer 16 with range of -32,768 to 32767: %v \n", signedInt16)
 
 	var signedInt32 int32 = 2147483647
-	fmt.Printf("\t Signed Integer 32 with range of -2,147,483,648 to 2147483647: %v \n", signedInt32)
+	fmt.Printf("\t Integer 32 with range of -2,147,483,648 to 2147483647: %v \n", signedInt32)
 
 	var signedInt64 int64 = 9223372036854775807
-	fmt.Printf("\t Signed Integer 64 with range of -9,223,372,036,854,775,808 to 9223372036854775807: %v \n", signedInt64)
-	fmt.Println("\t ====================================================")
+	fmt.Printf("\t Integer 64 with range of -9,223,372,036,854,775,808 to 9223372036854775807: %v \n", signedInt64)
 
+	fmt.Println("\t === Unsigned Integer ===")
 	var unSignedInt8 uint8 = 255
 	fmt.Printf("\t Unsigned Integer 8 with range of 0 to 255: %v \n", unSignedInt8)
 
@@ -69,8 +102,8 @@ func numericDataType() {
 
 	var unSignedInt64 uint64 = 18446744073709551615
 	fmt.Printf("\t Unsigned Integer 64 with range of 0 to 18446744073709551615: %v \n", unSignedInt64)
-	fmt.Println("\t ====================================================")
 
+	fmt.Println("\t === Float ===")
 	var float32 float32 = 18.1234567
 	fmt.Printf("\t Float 32 with max value of 7 decimal digits: %v \n", float32)
 
@@ -79,17 +112,80 @@ func numericDataType() {
 }
 
 func textDataType() {
+	fmt.Println("\t === String ===")
 	var string string = "Juan"
 	fmt.Printf("\t String data type: %v \n", string)
 
+	fmt.Println("\t === Character ===")
 	var character rune = 'J'
 	fmt.Printf("\t Character data type: %c \n", character)
 }
 
 func dataStructureDataType() {
+	names := []string{
+		"Red",
+		"Blue",
+	}
+	fmt.Println("\t === Slice ===")
+	for i, v := range names {
+		fmt.Printf("\t Index: %v, Value: %v \n", i, v)
+	}
 
+	persons := map[int]string{
+		1: "Red",
+		2: "Blue",
+	}
+	fmt.Println("\t === Map ===")
+	for k, v := range persons {
+		fmt.Printf("\t Key: %d Value: %s \n", k, v)
+	}
+
+	fmt.Println("\t === Struct ===")
+	type Person struct {
+		id   uint
+		Name string
+	}
+	person := Person{
+		1,
+		"Red",
+	}
+	fmt.Printf("\t %+v \n", person)
 }
 
-func specialDataType() {
+func decisionMaking() {
+	fmt.Println("\t === If ===")
+	const age uint8 = 15
+	if age < 20 {
+		fmt.Println("\t Executed because age is less than 20")
+	} else {
+		fmt.Println("\t Executed because age is greater than 20")
+	}
 
+	fmt.Println("\t === If Else ===")
+	if age < 20 {
+		fmt.Println("\t Executed because age is less than 20")
+	} else if age < 25 {
+		fmt.Println("\t Executed because age is less than 25")
+	} else {
+		fmt.Println("\t Executed because age is more than 25")
+	}
+}
+
+func controlFlow() {
+	fmt.Println("\t === FOR I ===")
+	for i := 1; i < 10; i++ {
+		fmt.Print(i)
+	}
+	fmt.Println()
+
+	fmt.Println("\n \t === FOR EACH ===")
+	numbers := []int{
+		1,
+		2,
+		3,
+	}
+	for _, number := range numbers {
+		fmt.Print(number)
+	}
+	fmt.Println()
 }
